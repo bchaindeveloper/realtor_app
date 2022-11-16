8ae7d9ff614bc65f3a8e8478990cf70463c10e01
bchaindeveloper <bcdev3@protonmail.com>
2022-11-16 14:15:15 -0500

    Assets

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button} from '@chakra-ui/react';
+import Property from '../components/Property';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
@@ -17,7 +19,8 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
 )
 
 
-export default function Home() {
+export default function Home({ propertiesForSale, propertiesForRent }) {
+  console.log(propertiesForSale, propertiesForRent)
   return (
     <Box>
       <h1>Hello World</h1>
@@ -32,7 +35,7 @@ export default function Home() {
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
       />
       <Flex flexWrap="wrap">
-
+      {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
       </Flex>
       <Banner
         purpose='BUY A HOME'
@@ -44,7 +47,19 @@ export default function Home() {
         linkName='/search?purpose=for-sale'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
       />
-      
+      {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
     </Box>
   )
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
