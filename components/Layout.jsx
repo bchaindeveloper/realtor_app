b414ba918725dd00fab83eda6e5fa72b44cbeca0
bchaindeveloper <bcdev3@protonmail.com>
2022-11-16 15:52:35 -0500

    Home Page

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,25 +1,25 @@
 import Head from 'next/head';
-import {Box} from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 
 
-const Layout = () => {
+const Layout = ({ children }) => (
     <>
     <Head>
         <title>Real Estate</title>
     </Head>
-    <Box maxWidth="1200px" m="auto">
+    <Box maxWidth="1280px" m="auto">
         <header>
             Navbar
         </header>
         <main>
             {children}
         </main>
-        <Footer>
+        <footer>
             Footer
-        </Footer>
+        </footer>
     </Box>
     </>
-};
+);
 
 export default Layout;
