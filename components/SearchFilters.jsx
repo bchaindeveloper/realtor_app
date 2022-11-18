85ddaadf7c1960af049e4856f919a643ca7c81c1
bchaindeveloper <bcdev3@protonmail.com>
2022-11-18 12:51:35 -0500

    Search

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { MdCancel } from 'react-icons/md';
+import Image from 'next/image';
+
+const SearchFilters = () => {
+    const [filters, setFilters] = useState({});
+
+    return (
+        <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+            SearchFilters
+        </Flex>
+    )
+}
+
+export default SearchFilters;
